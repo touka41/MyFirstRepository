8ddedc67b1381763e2281680168a67491394381b
Philip Joshua Mendoza <joshanicetemendoza@gmail.com>
2020-09-28 11:59:56 +0800

    Added mvc to application

--- Stalwart/Stalwart/Startup.cs
+++ Stalwart/Stalwart/Startup.cs
@@ -16,6 +16,7 @@ namespace Stalwart
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +27,20 @@ namespace Stalwart
                 app.UseDeveloperExceptionPage();
             }
 
+
             app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
